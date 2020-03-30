cbdfc6eff073aebf292f42fed3418b80763009bd
Orin Walker <orin.walker@gmail.com>
2020-03-30 14:00:56 -0400

    Just before adding validation

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,46 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true,
+    });
+
+    // this is the form model, it tracks the form value and state
+    // this is not the same as the data model (customer class)
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true),
+    // });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData() {
+    // this.customerForm.setValue({
+    //   firstName: 'Jack',
+    //   lastName: 'Harkness',
+    //   email: 'jack@torchwood.com',
+    //   sendCatalog: true,
+    // });
+
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+    });
   }
 }
