5478687301f77666d98156f7708941c72ba09723
Orin Walker <orin.walker@gmail.com>
2020-03-31 15:51:51 -0400

    final version

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+
 import { debounceTime } from 'rxjs/operators';
 
 import { Customer } from './customer';
@@ -20,11 +21,12 @@ function ratingRange(min: number, max: number): ValidatorFn {
 function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   const emailControl = c.get('email');
   const confirmControl = c.get('confirmEmail');
-  console.log(`Inside emailMatcher, email: ${emailControl.value}, confirmEmail: ${confirmControl.value}`);
 
   if (emailControl.pristine || confirmControl.pristine) {
     return null;
   }
+  console.log(`Inside emailMatcher, email: ${emailControl.value}, confirmEmail: ${confirmControl.value}`);
+
   if (emailControl.value === confirmControl.value) {
     return null;
   }
@@ -48,24 +50,23 @@ export class CustomerComponent implements OnInit {
   private validationMessages = {
     required: 'Please enter your email address.',
     email: 'Please enter a valid email address.'
-  }
+  };
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      emailGroup: this.fb.group( {
+      emailGroup: this.fb.group({
         email: ['', [Validators.required, Validators.email]],
         confirmEmail: ['', Validators.required],
-      }, { validator: emailMatcher} ),
+      }, { validator: emailMatcher }),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
       sendCatalog: true,
-      addresses: this.fb.array([ this.buildAddress() ])
+      addresses: this.fb.array([this.buildAddress()])
     });
 
     this.customerForm.get('notification').valueChanges.subscribe(
@@ -73,14 +74,44 @@ export class CustomerComponent implements OnInit {
     );
 
     const emailControl = this.customerForm.get('emailGroup.email');
-    emailControl.valueChanges
-      .pipe(debounceTime(1000))
-      .subscribe(
-       value => this.setMessage(emailControl)
-      );
-      }
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
+  }
 
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
 
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: ['', Validators.required],
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      emailGroup: { email: 'jack@torchwood.com', confirmEmail: 'jack@torchwood.com' }
+    });
+    const addressGroup = this.fb.group({
+      addressType: 'work',
+      street1: 'Mermaid Quay',
+      street2: '',
+      city: 'Cardiff Bay',
+      state: 'CA',
+      zip: ''
+    });
+    this.customerForm.setControl('addresses', this.fb.array([addressGroup]));
+  }
 
   save() {
     console.log(this.customerForm);
@@ -105,32 +136,4 @@ export class CustomerComponent implements OnInit {
     phoneControl.updateValueAndValidity();
   }
 
-  addAddress(): void {
-    this.addresses.push(this.buildAddress());
-  }
-
-  buildAddress(): FormGroup {
-    return this.fb.group({
-      addressType: 'home',
-      street1: '',
-      street2: '',
-      city: '',
-      state: '',
-      zip: ''
-    });
-  }
-
-  populateTestData() {
-    // this.customerForm.setValue({
-    //   firstName: 'Jack',
-    //   lastName: 'Harkness',
-    //   email: 'jack@torchwood.com',
-    //   sendCatalog: true,
-    // });
-
-    this.customerForm.patchValue({
-      firstName: 'Jack',
-      lastName: 'Harkness',
-    });
-  }
 }
